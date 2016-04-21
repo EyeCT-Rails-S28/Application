2591c05402cc1cbff69749d196182de91e2f4cbf
Lesley Vente <l.vente@student.fontys.nl>
2016-04-21 15:49:13 +0200

    User context implementation

--- EyeCT4Rails/EyeCT4RailsDatabase/UserSQLContext.cs
+++ EyeCT4Rails/EyeCT4RailsDatabase/UserSQLContext.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using EyeCT4RailsDatabase.Models;
 using EyeCT4RailsLib;
 using EyeCT4RailsLib.Enums;
+using Oracle.ManagedDataAccess.Client;
 
 namespace EyeCT4RailsDatabase
 {
@@ -13,12 +15,35 @@ namespace EyeCT4RailsDatabase
     {
         public User CreateUser(string name, string password, string email, Role role)
         {
-            throw new NotImplementedException();
+            OracleConnection connection = Database.Instance.Connection;
+            OracleCommand command = new OracleCommand("", connection);
+            command.CommandType = CommandType.StoredProcedure;
+
+            command.Parameters.Add(new OracleParameter("p_role", OracleDbType.Varchar2).Value = Convert.ToString(role));
+            command.Parameters.Add(new OracleParameter("p_name", OracleDbType.Varchar2).Value = name);
+            command.Parameters.Add(new OracleParameter("p_email", OracleDbType.Varchar2).Value = email);
+            command.Parameters.Add(new OracleParameter("p_password", OracleDbType.Varchar2).Value = password);
+
+            int id = Convert.ToInt32(command.ExecuteScalar());
+            return new User(id, name, email, role);
         }
 
         public User LoginUser(string email, string password)
         {
-            throw new NotImplementedException();
+            OracleConnection connection = Database.Instance.Connection;
+            OracleCommand command = new OracleCommand("", connection);
+            command.CommandType = CommandType.StoredProcedure;
+
+            command.Parameters.Add(new OracleParameter("p_email", OracleDbType.Varchar2).Value = email);
+            command.Parameters.Add(new OracleParameter("p_password", OracleDbType.Varchar2).Value = password);
+
+            OracleDataReader reader = command.ExecuteReader();
+            reader.Read();
+
+            int id = reader.GetInt32(0);
+            string name = reader.GetString(1);
+            Role role = (Role) Enum.Parse(typeof (Role), reader.GetString(2));
+            return new User(id, name, email, role);
         }
     }
 }
