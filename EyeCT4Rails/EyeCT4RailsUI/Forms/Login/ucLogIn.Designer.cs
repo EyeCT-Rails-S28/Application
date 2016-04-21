0ab14f5c854b0b87adecfe7e44c075dee6528c9e
jandie <eidnaj@live.nl>
2016-04-21 10:40:16 +0200

    Login appearance changed

--- EyeCT4Rails/EyeCT4RailsUI/Forms/Login/ucLogIn.Designer.cs
+++ EyeCT4Rails/EyeCT4RailsUI/Forms/Login/ucLogIn.Designer.cs
@@ -29,18 +29,26 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ucLogIn));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.lblEmail = new System.Windows.Forms.Label();
             this.lblPassword = new System.Windows.Forms.Label();
             this.tbEmail = new System.Windows.Forms.TextBox();
-            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.tbPassword = new System.Windows.Forms.TextBox();
             this.btnLogIn = new System.Windows.Forms.Button();
+            this.pbLock = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLock)).BeginInit();
             this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
             // lblEmail
             // 
             this.lblEmail.AutoSize = true;
-            this.lblEmail.Location = new System.Drawing.Point(3, 16);
+            this.lblEmail.Location = new System.Drawing.Point(157, 28);
             this.lblEmail.Name = "lblEmail";
             this.lblEmail.Size = new System.Drawing.Size(35, 13);
             this.lblEmail.TabIndex = 0;
@@ -49,7 +57,7 @@
             // lblPassword
             // 
             this.lblPassword.AutoSize = true;
-            this.lblPassword.Location = new System.Drawing.Point(3, 55);
+            this.lblPassword.Location = new System.Drawing.Point(157, 67);
             this.lblPassword.Name = "lblPassword";
             this.lblPassword.Size = new System.Drawing.Size(56, 13);
             this.lblPassword.TabIndex = 1;
@@ -57,19 +65,14 @@
             // 
             // tbEmail
             // 
-            this.tbEmail.Location = new System.Drawing.Point(66, 13);
+            this.tbEmail.Location = new System.Drawing.Point(220, 25);
             this.tbEmail.Name = "tbEmail";
             this.tbEmail.Size = new System.Drawing.Size(100, 20);
             this.tbEmail.TabIndex = 2;
             // 
-            // contextMenuStrip1
-            // 
-            this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
-            // 
             // tbPassword
             // 
-            this.tbPassword.Location = new System.Drawing.Point(66, 52);
+            this.tbPassword.Location = new System.Drawing.Point(220, 64);
             this.tbPassword.Name = "tbPassword";
             this.tbPassword.PasswordChar = '*';
             this.tbPassword.Size = new System.Drawing.Size(100, 20);
@@ -77,7 +80,7 @@
             // 
             // btnLogIn
             // 
-            this.btnLogIn.Location = new System.Drawing.Point(66, 90);
+            this.btnLogIn.Location = new System.Drawing.Point(220, 102);
             this.btnLogIn.Name = "btnLogIn";
             this.btnLogIn.Size = new System.Drawing.Size(100, 23);
             this.btnLogIn.TabIndex = 6;
@@ -85,29 +88,41 @@
             this.btnLogIn.UseVisualStyleBackColor = true;
             this.btnLogIn.Click += new System.EventHandler(this.btnLogIn_Click);
             // 
+            // pbLock
+            // 
+            this.pbLock.Image = ((System.Drawing.Image)(resources.GetObject("pbLock.Image")));
+            this.pbLock.Location = new System.Drawing.Point(0, 3);
+            this.pbLock.Name = "pbLock";
+            this.pbLock.Size = new System.Drawing.Size(141, 138);
+            this.pbLock.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbLock.TabIndex = 7;
+            this.pbLock.TabStop = false;
+            // 
             // ucLogIn
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pbLock);
             this.Controls.Add(this.btnLogIn);
             this.Controls.Add(this.tbPassword);
             this.Controls.Add(this.tbEmail);
             this.Controls.Add(this.lblPassword);
             this.Controls.Add(this.lblEmail);
             this.Name = "ucLogIn";
-            this.Size = new System.Drawing.Size(185, 131);
+            this.Size = new System.Drawing.Size(357, 153);
+            ((System.ComponentModel.ISupportInitialize)(this.pbLock)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.Label lblEmail;
         private System.Windows.Forms.Label lblPassword;
         private System.Windows.Forms.TextBox tbEmail;
-        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.TextBox tbPassword;
         private System.Windows.Forms.Button btnLogIn;
+        private System.Windows.Forms.PictureBox pbLock;
     }
 }
