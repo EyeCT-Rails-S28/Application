81aed04e1938a45910740e1629b8232f1a4de645
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2016-04-23 20:46:55 +0200

    Improved database no connection error handling.

--- EyeCT4Rails/EyeCT4RailsUI/Forms/Login/ucLogIn.cs
+++ EyeCT4Rails/EyeCT4RailsUI/Forms/Login/ucLogIn.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
 using EyeCT4RailsLib;
-using EyeCT4RailsLib.Enums;
 using EyeCT4RailsLogic;
 
 namespace EyeCT4RailsUI.Forms.Login
@@ -27,8 +26,13 @@ namespace EyeCT4RailsUI.Forms.Login
             {
                 Console.WriteLine(ex.Message);
 
+                string message = ex.InnerException.Message == "Connection request timed out"
+                    ? "No database connection"
+                    : ex.Message;
+
+                MessageBox.Show(message);
                 //test
-                LoginSucceeded?.Invoke(new User(1, "test", "test@test.test", Role.Administrator), new EventArgs());
+                //LoginSucceeded?.Invoke(new User(1, "test", "test@test.test", Role.Administrator), new EventArgs());
             }
         }
     }
