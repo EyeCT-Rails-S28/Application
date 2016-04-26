5edb6981eb0a8d9787c7367beeac27fe8b168f14
Lesley Vente <l.vente@student.fontys.nl>
2016-04-26 12:16:16 +0200

    Easier configuration of database connection

---
+++ EyeCT4Rails/EyeCT4RailsDatabase/DatabaseConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EyeCT4RailsDatabase
+{
+    internal class DatabaseConfiguration
+    {
+        public const string DATABASE_USERNAME = "PTS36";
+        public const string DATABASE_PASSWORD = "PTS36";
+        public const string DATABASE_SERVICE = "XE";
+        public const string DATABASE_HOST = "172.20.36.2";
+        public const int DATABASE_PORT = 1521;
+
+
+        private DatabaseConfiguration()
+        {
+        }
+    }
+}
