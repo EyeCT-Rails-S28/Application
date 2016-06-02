1d51ccb8479aad4875fb6bed6bfd44b18276933b
jandie <eidnaj@live.nl>
2016-06-02 12:40:17 +0200

    Maintenance functionality added

--- EyeCT4Rails/EyeCT4RailsASP/Controllers/CleanupController.cs
+++ EyeCT4Rails/EyeCT4RailsASP/Controllers/CleanupController.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Web.Mvc;
-using Antlr.Runtime.Misc;
-using EyeCT4RailsLib;
+using EyeCT4RailsLib.Classes;
 using EyeCT4RailsLib.Enums;
 using EyeCT4RailsLogic;
 
@@ -18,20 +17,7 @@ namespace EyeCT4RailsASP.Controllers
 
         public ActionResult Overview()
         {
-            Line line = new Line(1);
-            Tram tram = new Tram(0, TramType.Combino, Status.Remise, line, false);
-            Job job = new Job(0, DateTime.Now, false, JobType.Cleanup, JobSize.Small, tram, null);
-
-            
-            //List<Job> jobs = CleanupRepository.Instance.GetSchedule();
-            List<Job> jobs = new ListStack<Job>();
-            jobs.Add(job);
-            jobs.Add(job);
-            jobs.Add(job);
-            jobs.Add(job);
-            jobs.Add(job);
-
-            //test dummy jobs
+            List<Job> jobs = CleanupRepository.Instance.GetSchedule();
 
             ViewBag.Jobs = jobs;
 
