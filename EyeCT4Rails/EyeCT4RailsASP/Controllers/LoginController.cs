e5b6991654146f085c8eb32d5b1ff7908567fae1
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2016-06-02 13:55:49 +0200

    Finalize login form.

--- EyeCT4Rails/EyeCT4RailsASP/Controllers/LoginController.cs
+++ EyeCT4Rails/EyeCT4RailsASP/Controllers/LoginController.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
+using EyeCT4RailsLib.Classes;
+using EyeCT4RailsLib.Enums;
+using EyeCT4RailsLogic;
 
 namespace EyeCT4RailsASP.Controllers
 {
@@ -7,6 +11,40 @@ namespace EyeCT4RailsASP.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string email, string password)
+        {
+            User user;
+
+            try
+            {
+                user = UserRepository.Instance.LoginUser(email, password);
+            }
+            catch (Exception e)
+            {
+                ViewBag.Exception = e.Message;
+                return View();
+            }
+            
+            Session["User"] = user;
+
+            switch (user.Function.Role)
+            {
+                case Role.Administrator:                
+                case Role.DepotManager:
+                    return RedirectToAction("Index", "Depot");
+                case Role.Mechanic:
+                    return RedirectToAction("Index", "Maintenance");
+                case Role.Cleanup:
+                    return RedirectToAction("Index", "Cleanup");
+                case Role.Driver:
+                    return RedirectToAction("Index", "Ride");
+                default:
+                    return Redirect("http://man-man-man.nl/");
+            }
+
         }
     }
 }
