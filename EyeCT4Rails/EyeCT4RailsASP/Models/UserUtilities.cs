3315b82f5b3fdf5c0bb3efa9f4da255215979711
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2016-06-02 15:44:47 +0200

    Revert "Revert "Added a user utilities class.""
    This reverts commit d921207f72c23b274cc19d36d0a0432c9cbe8630.

---
+++ EyeCT4Rails/EyeCT4RailsASP/Models/UserUtilities.cs
@@ -0,0 +1,55 @@
+﻿using EyeCT4RailsLib.Classes;
+using EyeCT4RailsLib.Enums;
+
+namespace EyeCT4RailsASP.Models
+{
+    public class UserUtilities
+    {
+        public static string GetControllerName(User user)
+        {
+            string ret;
+
+            switch (user?.Function.Role)
+            {
+                case Role.Administrator:
+                case Role.DepotManager:
+                    ret = "Depot";
+                    break;
+                case Role.Mechanic:             
+                    ret = "Maintenance";
+                    break;
+                case Role.Cleanup:
+                    ret = "Cleanup";
+                    break;
+                case Role.Driver:
+                    ret = "Ride";
+                    break;
+                default:
+                    ret = "Login";
+                    break;
+            }
+
+            return ret;
+        }
+
+        public static string GetActionName(User user)
+        {
+            string ret;
+
+            switch (user?.Function.Role)
+            {
+                case Role.Mechanic:
+                    ret = "Overview";
+                    break;
+                case Role.Cleanup:
+                    ret = "Overview";
+                    break;
+                default:
+                    ret = "Index";
+                    break;
+            }
+
+            return ret;
+        }
+    }
+}
