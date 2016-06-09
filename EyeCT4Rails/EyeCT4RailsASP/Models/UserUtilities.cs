81cfa033264cf7c37a7492ebff8082ae7e7d819b
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2016-06-09 09:08:24 +0200

    Added comments and checkRight method

--- EyeCT4Rails/EyeCT4RailsASP/Models/UserUtilities.cs
+++ EyeCT4Rails/EyeCT4RailsASP/Models/UserUtilities.cs
@@ -1,10 +1,27 @@
-﻿using EyeCT4RailsLib.Classes;
+﻿using System.Web.ModelBinding;
+using EyeCT4RailsLib.Classes;
 using EyeCT4RailsLib.Enums;
 
 namespace EyeCT4RailsASP.Models
 {
     public class UserUtilities
     {
+        /// <summary>
+        /// Checks wheter the current user has a certain right.
+        /// </summary>
+        /// <param name="right">The right the user should posess.</param>
+        /// <param name="user">The user is question.</param>
+        /// <returns>A boolean that is true if and only if the user has the certain right.</returns>
+        public static bool CheckRight(Right right, User user)
+        {
+            return user?.HasRight(right) ?? false;
+        }
+
+        /// <summary>
+        /// Gets the standard controller name for a certain role.
+        /// </summary>
+        /// <param name="user">The current user.</param>
+        /// <returns>A string which is the controller name.</returns>
         public static string GetControllerName(User user)
         {
             string ret;
@@ -32,6 +49,11 @@ namespace EyeCT4RailsASP.Models
             return ret;
         }
 
+        /// <summary>
+        /// Gets the action name of the standard page of a role.
+        /// </summary>
+        /// <param name="user">The current user.</param>
+        /// <returns>A string which is the action name.</returns>
         public static string GetActionName(User user)
         {
             string ret;
