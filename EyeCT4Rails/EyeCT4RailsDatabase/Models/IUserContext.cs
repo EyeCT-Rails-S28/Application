b57c4956735efc7258450a0b1d0eb656807d6cdf
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2016-06-09 16:15:28 +0200

    Made passwords hashed and salted.

--- EyeCT4Rails/EyeCT4RailsDatabase/Models/IUserContext.cs
+++ EyeCT4Rails/EyeCT4RailsDatabase/Models/IUserContext.cs
@@ -13,7 +13,8 @@ namespace EyeCT4RailsDatabase.Models
         /// <param name="password">Password of the user.</param>
         /// <param name="email">Email of the user.</param>
         /// <param name="role">Role of the user.</param>
-        void CreateUser(string name, string password, string email, Role role);
+        /// <param name="salt">The salt of the user.</param>
+        void CreateUser(string name, string password, string email, Role role, string salt);
 
         /// <summary>
         /// Tries to login an user with the given email and password.
@@ -22,6 +23,13 @@ namespace EyeCT4RailsDatabase.Models
         /// <param name="password">Password of the user.</param>
         /// <returns></returns>
         User LoginUser(string email, string password);
+
+        /// <summary>
+        /// Gets the salt of an user.
+        /// </summary>
+        /// <param name="email">The email of the user.</param>
+        /// <returns>The salt if the user exists. Exception otherwise.</returns>
+        string GetSalt(string email);
 
         /// <summary>
         /// Gets an user from the database with the given id.
