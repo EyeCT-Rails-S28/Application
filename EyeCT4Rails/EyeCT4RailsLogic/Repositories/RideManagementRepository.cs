732d075e984bcac3d4f4b93fe91d420f0341d7fd
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2016-06-14 15:41:57 +0200

    Refactored ride controller.

--- EyeCT4Rails/EyeCT4RailsLogic/Repositories/RideManagementRepository.cs
+++ EyeCT4Rails/EyeCT4RailsLogic/Repositories/RideManagementRepository.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using EyeCT4RailsDatabase.Models;
 using EyeCT4RailsDatabase.SQLContexts;
+using EyeCT4RailsLib.Classes;
 using EyeCT4RailsLib.Enums;
 using EyeCT4RailsLogic.Exceptions;
-using EyeCT4RailsLogic.Utilities;
+using static EyeCT4RailsLogic.Utilities.ExceptionHandler;
+using static EyeCT4RailsLogic.Utilities.SectionUtil;
 
 // ReSharper disable MemberCanBeMadeStatic.Local
 
@@ -24,6 +26,90 @@ namespace EyeCT4RailsLogic.Repositories
         /// </summary>
         public static RideManagementRepository Instance => _instance ?? (_instance = new RideManagementRepository());
 
+
+        public string GetSection(int tramId, string status)
+        {
+            try
+            {
+                Depot depot = DepotManagementRepository.Instance.GetDepot("Havenstraat");
+
+                if (tramId < 0)
+                    throw new InvalidIdException("Tram ID mag niet leeg zijn.");
+
+                if (!depot.Trams.Exists(t => t.Id == tramId))
+                    throw new InvalidIdException($"Tram met ID: {tramId} bestaat niet.");
+
+                Track track = depot.Tracks.Find(t => t.Sections.Exists(s => s.Tram?.Id == tramId));
+                Section section;
+
+                //Tram is already reserved for a certain section.
+                if (track != null)
+                {
+                    section = track.Sections.Find(s => s.Tram?.Id == tramId);
+
+                    switch (status)
+                    {
+                        case "Maintenance":
+                            Instance.ChangeTramStatus(tramId, Status.Defect);
+                            break;
+                        case "Cleanup":
+                            Instance.ChangeTramStatus(tramId, Status.Schoonmaak);
+                            break;
+                        default:
+                            if (section.Tram.Status == Status.Dienst)
+                                Instance.ChangeTramStatus(tramId, Status.Remise);
+                            break;
+                    }
+                }
+                //If there is no reserved section the driver should wait for instruction.
+                else if (status == "Maintenance")
+                {
+                    Instance.ChangeTramStatus(tramId, Status.Gereserveerd);
+
+                    throw new SpecialActionException("Er is een speciale actie vereist van een beheerder, wacht op instructies.");
+                }
+                //Else find a new section for the tram.
+                else
+                {
+                    section = GetFreeSection(depot, depot.Trams.Find(t => t.Id == tramId).TramType);
+                    track = depot.Tracks.Find(t => t.Sections.Exists(s => s.Id == section.Id));
+
+                    if (track == null)
+                        throw new InvalidIdException("Fout bij ophalen van het spoornummer.");
+
+                    DepotManagementRepository.Instance.ReserveSection(tramId, section.Id);
+                    Instance.ChangeTramStatus(tramId, status == "None" ? Status.Remise : Status.Schoonmaak);
+                }
+
+                return $"{section.Id},{track.Id}";
+            }
+            catch (Exception e)
+            {
+                FilterOracleDatabaseException(e);
+                FilterCustomException(e);
+
+                if (e is SpecialActionException)
+                    throw;
+
+                throw new UnknownException("FATAL ERROR! EXTERMINATE! EXTERMINATE!");
+            }           
+        }
+
+        public string GetAssignedSection(int tramId)
+        {
+            Depot depot = DepotManagementRepository.Instance.GetDepot("Havenstraat");
+            Tram tram = depot.Trams.Find(t => t.Id == tramId);
+
+            if (tram == null || tram.Status == Status.Gereserveerd)
+                throw new InvalidIdException("Er is nog geen spoor toegewezen.");
+
+            Track track =
+                depot.Tracks.Find(t => t.Sections.Find(s => s.Tram != null && s.Tram.Id == tramId) != null);
+            Section section = track.Sections.Find(s => s.Tram != null && s.Tram.Id == tramId);
+
+            return $"{section.Id},{track.Id}";
+        }
+
         /// <summary>
         /// Changes the status of a tram. Dangerous code!
         /// </summary>
@@ -37,7 +123,7 @@ namespace EyeCT4RailsLogic.Repositories
             }
             catch (Exception e)
             {
-                ExceptionHandler.FilterOracleDatabaseException(e);
+                FilterOracleDatabaseException(e);
                 throw new UnknownException("FATAL ERROR! EXTERMINATE! EXTERMINATE!");
             }
         }      
